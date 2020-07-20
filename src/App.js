b4b226138d1bad6e8ee06644a61f118bd0e07dff
Massimiliano Ranauro <huckbit@gmail.com>
2020-07-20 17:20:21 +0100

    cards with code in site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.scss';
-import Card from './components/Card';
+import React from "react";
+import "./App.scss";
+import loadersData from "./data/loaders";
+import Card from "./components/Card";
 
 class App extends React.Component {
   render() {
+    const loadersComponents = loadersData.map((loader) => (
+      <Card key={loader.id} title={loader.name} spinnerMarkup={loader.markup} />
+    ));
     return (
       <div className="container">
         <div className="columns">
-          <div className="column">
-            <Card />
-          </div>
-          <div className="column">
-            <Card />
-          </div>
+          <div className="column">{loadersComponents}</div>
         </div>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,24 @@
 import React from "react";
+import ReactHtmlParser from "react-html-parser";
 
 class Card extends React.Component {
-  
   render() {
+    let html = this.props.spinnerMarkup;
+
     return (
       <div className="card">
-        <div className="card-image">
-        </div>
+        <div className="card-image"></div>
         <div className="card-content">
           <div className="media">
             <div className="media-content">
-              <p className="title is-4">John Smith</p>
-              <p className="subtitle is-6">@johnsmith</p>
+              <p className="title is-4">{this.props.title}</p>
             </div>
           </div>
           <div className="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            <a href="#">#css</a> <a href="#">#responsive</a>
+            {ReactHtmlParser(html)}
+            <a className="button is-link" href="/">
+              Source
+            </a>
           </div>
         </div>
       </div>

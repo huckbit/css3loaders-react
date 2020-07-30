fc7595c41694ab30dc7b76972b0cefda61d0c79c
Massimiliano Ranauro <huckbit@gmail.com>
2020-07-30 16:19:22 +0100

    continuos pagination

--- src/pages/Loader/index.js
+++ src/pages/Loader/index.js
@@ -6,18 +6,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 import loadersData from "../../data/loaders";
+import loaders from "../../data/loaders";
 
 export default class Loader extends Component {
   render() {
     const id = this.props.match.params.id;
-    const previous = id > 1 ? parseInt(id) - 1 : false;
-    const next = id < loadersData.length ? parseInt(id) + 1 : false;
+    const previous = id > 1 ? parseInt(id) - 1 : loadersData.length;
+    const next = id < loadersData.length ? parseInt(id) + 1 : 1;
     const loaderItem = loadersData.filter((element) => element.id.toString() === id);
     const { name, markup, css } = loaderItem[0];
     return (
       <DefaultLayout>
         <div className="section">
-          <div className="columns">
+          <div className="columns is-mobile">
             <div className="column is-2">
               {previous && (
                 <Link className="title is-2" to={{ pathname: `/loader/${previous}` }}>
@@ -25,15 +26,13 @@ export default class Loader extends Component {
                 </Link>
               )}
             </div>
-            <div className="column">
+            <div className="column has-text-centered">
               <h1 className="title is-1">{name}</h1>
             </div>
-            <div className="column is-2">
-              {next && (
-                <Link className="title is-2" to={{ pathname: `/loader/${next}` }}>
-                  <FontAwesomeIcon icon={faCaretRight} />
-                </Link>
-              )}
+            <div className="column is-2 has-text-right">
+              <Link className="title is-1 is-mobile-2" to={{ pathname: `/loader/${next}` }}>
+                <FontAwesomeIcon icon={faCaretRight} />
+              </Link>
             </div>
           </div>
           <div className="columns">
